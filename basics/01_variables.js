4e4285fa4ca12a5e80cb2e1a165e0588995b97b5
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-07-15 23:34:59 +0530

    basics of javascript

---
+++ basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "babayaga@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+
+let accountState = "Active";
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
