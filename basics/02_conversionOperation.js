ffa5d83bcc81d41f4c9811676470241616def3d4
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-07-25 12:55:04 +0530

    data type conversions

---
+++ basics/02_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = NaN
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(isNaN(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true  => 1; false => 0
+
+let isLoggedIn = "babai";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "string" => 1; "" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber, typeof stringNumber);
+
+// *********************** Operations ********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log((3 + 4 * 8) % 4);
+
