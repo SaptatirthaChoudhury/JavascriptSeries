ba5489c784df08098bff94d4d337f5948cda8b0b
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-10-19 14:29:40 +0530

    numbers and maths

---
+++ basics/numbersAndMaths.js
@@ -0,0 +1,22 @@
+const score = 100;
+//console.log(score.toString());
+
+const balance = new Number(2000);
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const number = 123.456789;
+const formattedNumber = number.toPrecision(6);
+//console.log(formattedNumber); 
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//_________________________ Maths Library____________//
+
+console.log((Math.random() * 10) + 99);
+
+
+
