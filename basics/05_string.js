bd8b2355d3b3198c3bdef9e95c39a55091fc9db6
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-10-23 21:24:06 +0530

    chapter string

---
+++ basics/05_string.js
@@ -0,0 +1,43 @@
+const name = "babayaga"
+const currencyType = "$"
+
+// console.log(`${name} using ${currencyType}`);
+
+// console.log(typeof currencyType);
+
+const gameName = new String("callOfDuty")
+
+// console.log(gameName[6]);
+// console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('f'));
+
+const newString = gameName.substring(0, 4) // It works from 0 to 4-1
+//console.log(newString);
+
+const anotherString = gameName.slice(-6, 5);
+//console.log(anotherString);
+
+const newStringOne = "   hitesh   "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhury"
+
+//console.log(url.replace("%20", "-"));
+
+//console.log(url.includes("http"));
+
+let escapedString = "This is an example of escaping special characters: \"@\"  \'hi!\' ";
+//console.log(escapedString);
+
+let path = "C:\\MyFiles\\Document.txt";
+//console.log(path); 
+
+let multiLineString = "Line 1\nLine 2";
+console.log(multiLineString);
+
