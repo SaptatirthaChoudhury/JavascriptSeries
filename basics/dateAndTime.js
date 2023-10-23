7c6c7f00c3e9592d9069f1a82c9e0a723e7e8460
SaptatirthaChoudhury <saptatirtha@gmail.com>
2023-10-23 21:24:50 +0530

    date and time

---
+++ basics/dateAndTime.js
@@ -0,0 +1,36 @@
+//let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+// console.log(typeof myDate);
+
+//let myCreatedDate = new Date("2023-10-22")
+//let todaysDate = new Date("2023-10-10")
+// let timeDifference = myCreatedDate.getDate() - todaysDate.getDate();
+//console.log(`${timeDifference} days`);
+// _______________ OR ___________________
+//let timeDifference = myCreatedDate - todaysDate;
+//let numberOfDays = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+//console.log(`${numberOfDays} days`);
+//_______________________ End _____________________________
+
+// let thatDate = new Date("10-14-2023")
+// let getTimeOnThatDate = thatDate.getTime();
+// //console.log(getTimeOnThatDate);
+
+// let currentTimeStamp = Date.now();
+// let timeDifference = currentTimeStamp - getTimeOnThatDate;
+
+//console.log(Math.floor(timeDifference/(1000*60*60*24)));
+
+//_________________ End _________________
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
